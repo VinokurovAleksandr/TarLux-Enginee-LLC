6ea4fb182be867870a8d80520d722899377ff9de
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2024-03-02 13:17:58 +0200

    add parnters

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.clients-image__slider', {
+  loop: true,
+  
+  slidesPerView: 4,
+  watchOverflow: true,
+  freeMode: true,
+  initialSlide: 1,
+  spaceBetween: 30,
+
+     
+    breakpoints: {
+        320: {
+          slidesPerView: 2,
+        },
+        480: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView:4,
+        }
+  },
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-block__arrow.swiper-button-next',
+    prevEl: '.slider-block__arrow.swiper-button-prev',
+    },
+      
+  
+  
+});
