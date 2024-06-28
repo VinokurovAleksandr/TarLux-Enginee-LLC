9278222cee4c77a695a8b3375beb0fcf76c1e41b
Oleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2024-06-28 10:29:19 +0300

    edits

--- js/cooperation.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function () {
-    scrollY > 0 ? document.querySelector('.header')
-      .classList.add('scroll') :
-      document.querySelector('header')
-        .classList.remove('scroll')
-  });
